f81d91f5bf921c3285c1ec02203fbc36f045b0f2
Prince Sly <princeslysoft@gmail.com>
2020-08-20 03:18:44 +0100

    third commit

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-dark" style="background-color: #800080;">
+        <div class="container">
+            <a href="#" class="navbar-brand">Lara Articles</a>
+        </div>
+    </nav>
+</template>
